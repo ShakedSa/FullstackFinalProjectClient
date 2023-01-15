1aa50ff0d6bd64cd23dab5ac5a425e263a426e60
Shaked <sabagshaked99@gmail.com>
2023-01-15 22:43:12 +0200

    update api source

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,4 +1,4 @@
-const server = "https://snmgarage-api.onrender.com/"
+const server = "https://snmgarage-api.onrender.com"
 const devPort = "3232";
 const API = "/api";
 const ServerAPI = `${server}${API}`;
